0a9b20eab247d9475f1a6b0896f7a8229210c7f0
Jakub Komosa <qbakom@hotmail.com>
2016-03-27 22:20:46 +0200

    algorytmy

--- Course/Part1/Program.cs
+++ Course/Part1/Program.cs
@@ -10,7 +10,101 @@ namespace Part1
     {
         static void Main(string[] args)
         {
+            
+            int a = 25;
+            int b = 23;
+            int c = 37;
+
+            int njw = znajdzNajwieksza(a, b, c);
+            Console.WriteLine(njw);
             Console.ReadKey();
+        }
+
+        static int znajdzdruga(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                {
+
+                    if (b > c)
+                    {
+                        return b;
+                        //Console.WriteLine("1a,2b,3c");
+                    }
+                    else
+                    {
+                        return c;
+                        // Console.WriteLine("1a,2c,3b");
+                    }
+                }
+                return a;
+                //Console.WriteLine("1c,2a,3b");
+            }
+            else
+            {
+                if (b > c)
+                {
+                    if (c > a)
+                    {
+
+                        return c;
+                        //Console.WriteLine("1b,2c,3a");
+                    }
+                    return a;
+                    //Console.WriteLine("1b,2a,3c");
+                }
+                else
+                {
+                    return b;
+                    //Console.WriteLine("1c,2b,3a");
+                }
+            }
+        }
+
+
+
+        static int znajdzNajwieksza(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                {
+
+                    if (b > c)
+                    {
+                        return a;
+                        //Console.WriteLine("1a,2b,3c");
+                    }
+                    else
+                    {
+                        return a;
+                       // Console.WriteLine("1a,2c,3b");
+                    }
+                }
+                return c;
+                //Console.WriteLine("1c,2a,3b");
+            }
+            else
+            {
+                if (b > c)
+                {
+                    if (c > a)
+                    {
+
+                        return b;
+                        //Console.WriteLine("1b,2c,3a");
+                    }
+                    return b;
+                    //Console.WriteLine("1b,2a,3c");
+                }
+                else
+                {
+                    return c;
+                    //Console.WriteLine("1c,2b,3a");
+                }
+            }
         }
     }
 }
+
