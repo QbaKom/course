c56e7b3233f9b115904f7fa8c43dabc4a9b558ef
Jakub Komosa <qbakom@hotmail.com>
2016-03-28 14:14:32 +0200

    Człowiek

---
+++ Course/KlasyIObiekty/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasyIObiekty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Czlowiek
+    {
+        public string KolorOczu { get; set; }
+
+        public string Plec { get; set; }
+
+        public int Wzrost { get; set; }
+
+        public int Waga { get; set; }
+
+        public bool Uczciwy { get; set; }
+
+        public string KolorSkory { get; set; }        
+    }
+}
